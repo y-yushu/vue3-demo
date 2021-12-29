39fa251e2980c12bd177a4a1c2f5ca489a7b3f02
七月十二 <2671255784@qq.com>
2021-12-29 22:21:38 +0800

    12-29 增加 pdf 多页下载

--- src/view/20211227/index.vue
+++ src/view/20211227/index.vue
@@ -5,6 +5,9 @@ import workerSrc from 'pdfjs-dist/build/pdf.worker.entry'
 // import Pdf from './test1.pdf'
 import Pdf from './test2.pdf'
 
+/**
+ * pdf渲染相关
+ */
 const pdflenght = ref(0)
 // 解析方式
 PdfJs.GlobalWorkerOptions.workerSrc = workerSrc
@@ -50,9 +53,54 @@ loadingTask.promise.then(pdf => {
   // 如果页码存在，则开始调用
   if (pdflenght.value) torender(1, pdflenght.value)
 })
+
+/**
+ * 页面显示相关
+ */
+const show = ref(false)
+// 鼠标移入移出
+const move = value => (show.value = value)
+// 点击打印
+const types = {
+  png: 'image/png',
+  jpg: 'image/jpeg',
+  webp: 'image/webp'
+}
+const print = () => {
+  console.log('触发打印')
+  for (let i = 1; i <= pdflenght.value; i++) {
+    const canvas = document.getElementById('pdf-canvas-' + i)
+    const a = document.createElement('a')
+    a.href = canvas.toDataURL(types.png)
+    a.download = '测试' + i
+    a.click()
+  }
+}
 </script>
 
 <template>
-  <button>解析</button>
+  <div class="header" @mouseover="move(true)" @mouseleave="move(false)">
+    <div v-if="show" class="buttons">
+      <el-button type="primary" @click="print()">测试</el-button>
+    </div>
+  </div>
   <canvas v-for="index in pdflenght" :key="index" :id="'pdf-canvas-' + index" />
 </template>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 40px;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  .buttons {
+    width: 100%;
+    height: 100%;
+    background-color: #c9d8cda8;
+    display: flex;
+    flex-direction: row-reverse;
+  }
+}
+</style>
