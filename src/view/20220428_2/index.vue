a82c64dfcfddca4e5db18c24e4facaab9f647fa3
七月十二 <2671255784@qq.com>
2022-04-28 15:47:11 +0800

    增加点击翻转效果

--- src/view/20220428_2/index.vue
+++ src/view/20220428_2/index.vue
@@ -1,9 +1,33 @@
+<script setup>
+import { ref, computed } from 'vue'
+const direc = ref(0)
+
+const tran = computed(() => {
+  return `transform: rotateY(${direc.value}deg);`
+})
+
+const overturn = () => {
+  direc.value = direc.value === 180 ? 0 : 180
+}
+</script>
+
 <template>
   <div class="body">
     <div class="box">
       <div class="flipper">
         <div class="front">123</div>
         <div class="back">456</div>
+      </div>
+    </div>
+    <div class="box_2">
+      <!-- <div class="flipper" :style="{ transform: 'rotateY(' + direc + 'deg)' }"> -->
+      <div class="flipper" :style="tran">
+        <div class="front">
+          <button @click="overturn()">翻转</button>
+        </div>
+        <div class="back">
+          <button @click="overturn()">再次翻转</button>
+        </div>
       </div>
     </div>
   </div>
@@ -28,6 +52,11 @@
   }
   .box:hover .flipper {
     transform: rotateY(180deg);
+  }
+  .box_2 {
+    width: 200px;
+    height: 240px;
+    perspective: 1000;
   }
   .flipper,
   .front,
